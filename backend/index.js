25df5d490f3dd9d3fe26d4d649bc1a420b74a2d8
urya2000 <suryaprakashmessi99@gmail.com>
2024-10-24 17:27:33 +0530

    serve create

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,10 @@
-
-console.log("surya")
-const express=require("express");
-const mongoose=require("mongoose")
-const cors=require("cors")
+console.log("surya");
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 
-
+app.listen("5000", () => {
+  console.log("server is running on port 5000");
+});
