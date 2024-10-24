7d0595b7b0efa8cc25cdfb9c4e0255950dcacc74
Karthick726 <karthickc726@gmail.com>
2024-10-24 17:54:09 +0530

    Sever run with therir port 8000

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,36 @@
-console.log("surya");
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 const app = express();
 
-app.listen("5000", () => {
-  console.log("server is running on port 5000");
+app.use(cookieParser());
+app.use(express.json());
+const corsOptions = {
+  origin: ["http://localhost:3001", "http://localhost:3000"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+};
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
+
+const mongo_url =
+  "mongodb+srv://karthickc726:Karthick@cluster0.04mzp.mongodb.net/AtoZ-drop-taxi?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose
+  .connect(mongo_url)
+  .then(() => {
+    console.log("db connect");
+    const port = 8000;
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log("error:" + error);
+  });
