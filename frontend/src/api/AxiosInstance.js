cb650f55d1b8e8d0659b97ba8f00fc67518e296b
urya2000 <suryaprakashmessi99@gmail.com>
2024-10-29 15:58:53 +0530

    update axiosInstance

--- frontend/src/api/AxiosInstance.js
+++ frontend/src/api/AxiosInstance.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 const AxiosInstance = axios.create({
-  baseURL: "http://localhost:8000",
+  // baseURL: "http://localhost:8000",
+  baseURL: "https://ato-z-drop-taxi-backend.vercel.app",
   withCredentials: true,
   timeout: 100000,
   headers: {
